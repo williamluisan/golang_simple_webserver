4aac5212cd3050fff31913ca2aa15aae715ef648
DL William <dlprogrammer18@gmail.com>
2022-06-09 00:05:06 +0800

    (WIP) Dynamic webserver

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+func loadConfig() {
+	// load configuration
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func main() {
+	// load configuration
+	loadConfig()
+	port := os.Getenv("WEBSERVER_PORT")
+
+	// API routes
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		uri := r.URL.String()
+		uriSplit := strings.Split(uri, "/")
+		mainPath := uriSplit[1]
+		if mainPath != "" {
+			http.FileServer(http.Dir("./static"))
+		} else {
+			fmt.Fprintf(w, "Should showing default index.html")
+		}
+	})
+
+	fmt.Println("Server is running on port " + port)
+
+	// Start server on port specified above
+	log.Fatal(http.ListenAndServe(port, nil))
+}
