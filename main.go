12d153e05e6820e1415b1461de5ca3706943db42
DL William <dlprogrammer18@gmail.com>
2022-06-11 00:27:19 +0800

    (FIX)

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
+	"path"
+	"path/filepath"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -18,22 +20,42 @@ func loadConfig() {
 	}
 }
 
+func webServerHandler(w http.ResponseWriter, r *http.Request) {
+	rootFolder := os.Getenv("ROOT_FOLDER")
+	filePath := "./" + rootFolder + r.URL.String()
+	_, fileName := path.Split(filePath)
+	fileNameExt := filepath.Ext(filePath)
+
+	// check request
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+	// check file name extension
+	if fileNameExt == "" {
+		// if no extension, default open index.html
+		filePath = filePath + "/index.html"
+	}
+	file, err := os.Open(filePath)
+	if err != nil {
+		fmt.Printf("file %s not found\n", filePath)
+		w.WriteHeader(http.StatusNotFound)
+		http.Error(w, "404 not found", http.StatusNotFound)
+		
+		return
+	}
+	defer file.Close()
+
+	http.ServeContent(w, r, fileName, time.Now(), file)
+}
+
 func main() {
 	// load configuration
 	loadConfig()
 	port := os.Getenv("WEBSERVER_PORT")
 
-	// API routes
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		uri := r.URL.String()
-		uriSplit := strings.Split(uri, "/")
-		mainPath := uriSplit[1]
-		if mainPath != "" {
-			http.ServeFile(w, r, mainPath)
-		} else {
-			http.ServeFile(w, r, "./index.html")
-		}
-	})
+	http.HandleFunc("/", webServerHandler)
 
 	fmt.Println("Server is running on port " + port)
 
